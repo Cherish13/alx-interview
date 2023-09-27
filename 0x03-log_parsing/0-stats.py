507f8d72c6a86396bc2a604e573ddd1ee8b77eed
Shinene <cherishlyte13@gmail.com>
2023-09-27 15:53:55 +0100

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,18 +3,20 @@
 Task: 0. Log Parsing
 File: 0x06-log_parsing/0-stats.py
 """
+
 import sys
 
 
 def print_stats(total_size, status_code_counts):
     """
-    This prints statistics at the beginning and every 10 lines
-    This will also be called on a Keyboard interruption
+    Print statistics at the beginning and every 10 lines.
+    This will also be called on a Keyboard interruption.
     """
     print("File size: {}".format(total_size))
     for status_code in sorted(status_code_counts.keys()):
         if status_code_counts[status_code] > 0:
             print("{}: {}".format(status_code, status_code_counts[status_code]))
+
 
 def parse_line(line):
     try:
@@ -26,6 +28,7 @@ def parse_line(line):
     except ValueError:
         pass
     return None, None
+
 
 total_size = 0
 status_code_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
